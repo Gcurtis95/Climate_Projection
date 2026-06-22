0e63ce00bd38ecd43047454051cc9e3f80f6063b
gcurtis95 <gcurtis154@gmail.com>
2026-06-22 17:27:38 +0100

    updated gitignore

---
+++ frontend-v2/lib/env.ts
@@ -0,0 +1,3 @@
+export const BACKEND_URL = process.env.BACKEND_URL ?? "http://127.0.0.1:8000";
+export const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX ?? "";
+export const ISITWATER_KEY = process.env.ISITWATER ?? "";
